119160826f0adb88c1394158f5bdb8c038ea929a
Joon Hwan Hong <49882424+Joon-Hwan-Hong@users.noreply.github.com>
2019-09-29 09:57:14 -0400

    Revert "Update run.py"
    This reverts commit 5906009331c1b7996c142b3621bce38bd7c217fe.

--- run.py
+++ run.py
@@ -1,92 +1,25 @@
 from __future__ import print_function, division
-from visualizer import Visualizer
-from pynput.keyboard import Key, Controller
+
 import sys
 import cv2
 import wrnchAI
 import pickle
+from visualizer import Visualizer
+from pynput.keyboard import Key, Controller
 import numpy as np
 import os
+from utils import do_action, find_largest, get_joints, normalize, difference
 
-# ini definitions
-REFERENCE_JOINTS = [14, 15]
-DIFFERENCE_THRESHOLD = 10
-SPECIAL_KEYS = {"left":Key.left, "right":Key.right, "up":Key.up, "down":Key.down, "space":Key.space}
-
-
-def get_pose():
-    print('Opening webcam...')
-    cap = cv2.VideoCapture(webcam_index)
-
-    if not cap.isOpened():
-        sys.exit('Cannot open webcam.')
-
-    for i in range(10):
-
-        ret, frame = cap.read()
-
-        if frame is not None:
-            estimator.process_frame(frame, options)
-            humans2d = estimator.humans_2d()
-
-            visualizer.draw_image(frame)
-            for human in humans2d:
-                joints = human.joints()
-
-                visualizer.draw_points(joints)
-                visualizer.draw_lines(joints, bone_pairs)
-
-            visualizer.show()
-
-        key = cv2.waitKey(1)
-        # print(key)
-
-        if key & 255 == 27:
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-    return joints
-
-
-def do_action(keyboard, key):
-    if key in SPECIAL_KEYS.keys():
-        keyboard.press(SPECIAL_KEYS[key])
-    else:
-        keyboard.press(key)
-
-
-def similarity(old_joints, new_joints, imp_joints):
-    # diff = np.zeros(len(imp_joints))
-    old_diff = []
-    new_diff = []
-    # comparison of differences from head location x and y coordinates
-    for joint in imp_joints:
-        old_diff.append(old_joints[2*joint] - old_joints[8])
-        old_diff.append(old_joints[2*joint + 1] - old_joints[9])
-        new_diff.append(new_joints[2*joint] - new_joints[8])
-        new_diff.append(new_joints[2*joint + 1] - new_joints[9])
-    simm_score = 0
-
-    # if within 5% similarity of the original coordinate, increase the similarity score
-    for index, coordinate_diff in enumerate(new_diff):
-        if (0.95*old_diff[index]) < coordinate_diff < (1.05*old_diff[index]):
-            simm_score += coordinate_diff*10
-
-    return simm_score
-
-
-# check for proper console input
+print('Start')
 num_args = len(sys.argv)
-if num_args < 3 or num_args > 4:
+if num_args < 4 or num_args > 5:
     sys.exit("Usage: python run.py MODEL_DIR/ ACTION_FILE camera_index")
 
-if num_args == 4:
-    webcam_index = int(sys.argv[3])
+if num_args == 5:
+    webcam_index = int(sys.argv[4])
 else:
     webcam_index = 0
 
-# set up wrenchAI vision
 params = wrnchAI.PoseParams()
 params.bone_sensitivity = wrnchAI.Sensitivity.high
 params.joint_sensitivity = wrnchAI.Sensitivity.high
@@ -111,7 +44,6 @@ joint_definition = estimator.human_2d_output_format()
 bone_pairs = joint_definition.bone_pairs()
 visualizer = Visualizer()
 
-# activate keyboard functionality and check if action data exists
 if not os.path.isfile(sys.argv[2]):
     print('Saved Actions File not found')
 
@@ -121,7 +53,8 @@ with open(sys.argv[2], 'rb') as f:
 
 keyboard = Controller()
 
-# run
+run_last = [False] * len(saved_actions)
+
 while True:
     ret, frame = cap.read()
 
@@ -131,23 +64,27 @@ while True:
         visualizer.draw_image(frame)
 
         # Check for main person
-        for human in humans2d:
+        if len(humans2d) > 0:
+            human = humans2d[find_largest(humans2d)]
             joints = human.joints()
             visualizer.draw_points(joints)
             visualizer.draw_lines(joints, bone_pairs)
 
-            joints = joints - np.tile(joints[REFERENCE_JOINTS], int(len(joints) / 2))
+            norm_joints = normalize(joints)
 
-            # load and determine similarity by comparing to saved pose
             for i in range(len(saved_actions)):
                 key, indices, joints_pos = saved_actions[i]
                 # print(key, indices, joints_pos)
-                sim_score = similarity(joints_pos, joints, indices)
-                print(abs(sim_score))
-                if abs(sim_score) > DIFFERENCE_THRESHOLD:
-                    do_action(keyboard, key)
+                error = difference(joints_pos, norm_joints, indices)
+                print(key, error)
+                if abs(error) < DIFFERENCE_THRESHOLD:
+                    if (sys.argv[3] in ["-r", "-R"]) or (not run_last[i]):
+                        do_action(keyboard, key)
+                        run_last[i] = True
+                else:
+                    run_last[i] = False
 
-        visualizer.show()
+            visualizer.show()
     key = cv2.waitKey(1)
 
     if key & 255 == 27:
@@ -155,8 +92,3 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
-
